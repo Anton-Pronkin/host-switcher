9549973ee4e4c91b2cab84ded304386ed0dda6e7
Anton Pronkin <Anton_Pronkin@epam.com>
2021-03-31 01:09:11 +0300

    Add styles for EditHostArea component

--- src/components/EditHostArea/index.js
+++ src/components/EditHostArea/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { HandlerManager } from "../../utils/GlobalContext";
+import Button from "../Button";
 import style from "./index.css";
 
 export default function EditHostArea({ host, onHostChanged, onCancel }) {
@@ -7,11 +8,18 @@ export default function EditHostArea({ host, onHostChanged, onCancel }) {
     const [value, setValue] = useState(host ? host.value : "");
 
     return (
-        <div>
-            <div>Name:</div><input type="text" onChange={changeName} defaultValue={name} />
-            <div>Value:</div><input type="text" onChange={changeValue} defaultValue={value} />
-            <button onClick={saveHost}>Save host</button>
-            <button onClick={onCancel}>Cancel</button>
+        <div className={style.editHostArea}>
+            <div className={style.editHostArea_fieldset}>
+                <label for="name">Name:</label>
+                <input type="text" id="name" className={style.editHostArea_input} onChange={changeName} defaultValue={name} />
+
+                <label for="value">Value:</label>
+                <input type="text" id="value" className={style.editHostArea_input} onChange={changeValue} defaultValue={value} />
+            </div>
+            <div className={style.editHostArea_buttonsArea}>
+                <Button onClick={saveHost}>Save host</Button>
+                <Button onClick={onCancel}>Cancel</Button>
+            </div>
         </div>
     );
 
