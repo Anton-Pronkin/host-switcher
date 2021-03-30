47877c862f6048a098bb0208fcdd1b92be355a7e
Anton Pronkin <Anton_Pronkin@epam.com>
2021-03-31 00:10:42 +0300

    Add style for DeleteHostArea component

--- src/components/DeleteHostArea/index.js
+++ src/components/DeleteHostArea/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { HandlerManager } from "../../utils/GlobalContext";
+import style from "./index.css";
 
 export default function DeleteHostArea({ host, onDelete, onCancel }) {
     return (
-        <div>
-            <div>Do you want to delete host "{host.name}" (URL: {host.value})?</div>
-            <button onClick={deleteHost}>Delete</button>
-            <button onClick={onCancel}>Cancel</button>
+        <div className={style.deleteHostArea}>
+            <div className={style.deleteHostArea_title}>Do you want to delete host "{host.name}" (URL: {host.value})?</div>
+            <div className={style.deleteHostArea_buttonsArea}>
+                <div onClick={deleteHost} className={style.deleteHostArea_button}>Delete</div>
+                <div onClick={onCancel} className={style.deleteHostArea_button}>Cancel</div>
+            </div>
         </div>
     );
 
