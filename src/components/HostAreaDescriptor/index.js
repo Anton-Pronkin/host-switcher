bdc8f66575b356b377a616e0e5d79dd0e941506d
Anton Pronkin <Anton_Pronkin@epam.com>
2021-04-02 22:24:43 +0300

    Fix some UI issues

--- src/components/HostAreaDescriptor/index.js
+++ src/components/HostAreaDescriptor/index.js
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { HandlerManager } from "../../utils/GlobalContext";
 import style from "./index.css";
 
 export default function HostAreaDescriptor({ host, onEditClick, onDeleteClick }) {
+    const [isMousePressed, setIsMousePressed] = useState(false);
+
+    const buttonClass = style.hostAreaDescriptor_button;
+
     return (
-        <div className={style.hostAreaDescriptor}>
-            <div className={style.hostAreaDescriptor_host} onClick={switchHost}>
+        <div className={style.hostAreaDescriptor} onMouseDown={downMouse} onMouseUp={upMouse} >
+            <div className={style.hostAreaDescriptor_host}>
                 <div className={style.hostAreaDescriptor_name}>{host.name}</div>
                 <div className={style.hostAreaDescriptor_value}>{host.value}</div>
             </div>
-            <div onClick={onEditClick} className={`${style.hostAreaDescriptor_button} ${style.hostAreaDescriptor_button__edit}`}></div>
-            <div onClick={onDeleteClick} className={`${style.hostAreaDescriptor_button} ${style.hostAreaDescriptor_button__delete}`}></div>
+            <div onClick={onEditClick} className={`${buttonClass} ${style.hostAreaDescriptor_button__edit}`}></div>
+            <div onClick={onDeleteClick} className={`${buttonClass} ${style.hostAreaDescriptor_button__delete}`}></div>
         </div>
     );
 
-    async function switchHost() {
-        await HandlerManager.switchHost(host);
+    function downMouse(event) {
+        if (checkActiveArea(event.target)) {
+            setIsMousePressed(true);
+        }
+    }
+
+    async function upMouse(event) {
+        if (isMousePressed) {
+            setIsMousePressed(false);
+
+            if (checkActiveArea(event.target)) {
+                await HandlerManager.switchHost(host);
+            }
+        }
+    }
+
+    function checkActiveArea(element) {
+        return !element.classList.contains(buttonClass);
     }
 }
